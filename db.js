bf2d3450032b8e723d55f03953f136d759153999
NotYael <danyaeldelacruz@gmail.com>
2025-04-29 16:47:12 +0800

    Initial Functionality

---
+++ db.js
@@ -0,0 +1,15 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+export const query = (text, params) => pool.query(text, params);
+export default pool;
---
+++ server.js
@@ -0,0 +1,114 @@
+import express from 'express';
+import pool from './db.js';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+/* 
+TODO: IMPLEMENT THE FOLLOWING
+DATABASE:
+[X] (GET) Get user by id
+[X] (POST) Add new user
+[X] (PUT) Update user details by id (name and email)
+[X] (PUT) Update user password by id
+[] (POST) Create new account balance for new users
+[] (GET) Get user's account balance by id
+[] (UPDATE) Update user's account balance by id after transaction
+[] CREATE TRANSACTION TABLE
+[] (GET) Get user's transaction list by id
+
+CHATGPT:
+[] Sent requests to ChatGPT API
+
+EMAIL:
+[] Send email to user for vouchers
+
+*/
+
+// Middleware 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+// THIS IS JUST TO TEST DB CONNECTION
+// app.get('/', async (req, res) => {
+//   try {
+//     const result = await pool.query('SELECT * FROM users;');
+//     res.json(result.rows);
+//   } catch (err) {
+//     console.log(`Error connecting to the database: ${err}`);
+//     res.status(500).json({ error: 'Error fetching users' });
+//   }
+// });
+
+
+// **** USERS *****
+// Add user
+app.post('/add_user', async (req, res) => {
+    const { id, name, password, email } = req.body;
+    try {
+      const queryText = "INSERT INTO users (id, name, password, email) VALUES ($1, $2, $3, $4)";
+      await pool.query(queryText, [id, name, password, email]);
+      res.send("User added successfully");
+    } catch (err) {
+      console.log(`Error adding user: ${err}`);
+      res.status(500).json({ error: 'Error Adding User' });
+    }
+});
+
+// Get user by id
+app.get('/user', async (req, res) => {
+    const { id } = req.query;
+    console.log(`Fetching user with id: ${id}`);
+    try {
+      const queryText = "SELECT * FROM users WHERE id = $1";
+      const result = await pool.query(queryText, [id]);
+      if (result.rows.length > 0) {
+        res.json(result.rows[0]);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    } catch (err) {
+        console.log(`Error fetching user: ${err}`);
+        res.status(500).json({ error: 'Error fetching user' });
+    }
+});
+
+app.put('/edit_user_details', async (req, res) => {
+    const { id, name, email } = req.body;
+    try {
+      const queryText = "UPDATE users SET name = $1, email = $2 WHERE id = $3";
+      await pool.query(queryText, [name, email, id]);
+      res.send("User updated successfully");
+    } catch (err) {
+      console.log(`Error updating user: ${err}`);
+      res.status(500).json({ error: 'Error Updating User' });
+    }
+});
+
+app.put('/edit_user_password', async (req, res) => {
+    const { id, password } = req.body;
+    try {
+      const queryText = "UPDATE users SET password = $1 WHERE id = $2";
+      await pool.query(queryText, [password, id]);
+      res.send("User password updated successfully");
+    } catch (err) {
+      console.log(`Error updating user: ${err}`);
+      res.status(500).json({ error: 'Error Updating User' });
+    }
+});
+
+// Get user details
+
+// **** ACCOUNT BALANCE *****
+app.get('/balance/')
+
+
+// **** TRANSACTIONS ***** 
+app.get('/transactions')
+
+
+
+app.listen(port, () => {
+  console.log(`Backend running on Port ${port}`);
+});
